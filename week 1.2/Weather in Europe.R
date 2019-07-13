a792a393e6d7d4f53ad2c99892d9558354b07989
yo-shi-mi <erin001tw@gmail.com>
2019-07-13 17:11:40 +0800

    cbind lowest and highest temp. in berlin

--- week 1.2/Weather in Europe.R
+++ week 1.2/Weather in Europe.R
@@ -1,8 +1,13 @@
-read_html("https://www.accuweather.com/en/de/berlin/10178/july-weather/178087")
+t1 = read_html("https://www.accuweather.com/en/de/berlin/10178/july-weather/178087")
 
 doc <- read_html("https://www.accuweather.com/en/de/berlin/10178/july-weather/178087")
-doc %>% html_nodes(".temp span")
-doc %>% html_nodes(".temp span") %>% html_text()
-t <- doc %>% html_nodes(".temp span") %>% html_text()
+doc %>% html_nodes(".large-temp")
+doc %>% html_nodes(".large-temp") %>% html_text()
+highest<- doc %>% html_nodes(".large-temp") %>% html_text()
 
-t[2] <- substr(t[2], 11, 13)
+doc <- read_html("https://www.accuweather.com/en/de/berlin/10178/july-weather/178087")
+doc %>% html_nodes(".small-temp")
+doc %>% html_nodes(".small-temp") %>% html_text()
+lowest<- doc %>% html_nodes(".small-temp") %>% html_text()
+
+t = cbind(highest,lowest)
