2f094645b6f1ad0306717e1baa6dded07cd3925d
yo-shi-mi <erin001tw@gmail.com>
2019-07-14 21:35:05 +0800

    merge all the data

--- week 1.2/Weather in Europe.R
+++ week 1.2/Weather in Europe.R
@@ -1,74 +1,123 @@
 doc <- read_html("https://www.accuweather.com/en/de/berlin/10178/month/178087?monyr=6/01/2019")
 doc %>% html_nodes(".actual .large-temp")
 doc %>% html_nodes(".actual .large-temp") %>% html_text()
-highest<- doc %>% html_nodes(".large-temp") %>% html_text()
+h.t.Berlin<- doc %>% html_nodes(".actual .large-temp") %>% html_text()
+
 
 doc <- read_html("https://www.accuweather.com/en/de/berlin/10178/month/178087?monyr=6/01/2019")
 doc %>% html_nodes(".small-temp")
 doc %>% html_nodes(".small-temp") %>% html_text()
-lowest<- doc %>% html_nodes(".small-temp") %>% html_text()
-lowest<- substr(lowest,2,4)
-View(lowest)
+l.t.Berlin<- doc %>% html_nodes(".small-temp") %>% html_text()
+l.t.Berlin<- substr(l.t.Berlin,2,4)
+
+
 doc <- read_html("https://www.accuweather.com/en/de/berlin/10178/month/178087?monyr=6/01/2019")
 doc %>% html_nodes(".date")
 doc %>% html_nodes(".date") %>% html_text()
 date<- doc %>% html_nodes(".date") %>% html_text()
+date<- substr(date,4,8) 
+date[7]<- " 6/01"
+date[8]<- " 6/02"
+date[9]<- " 6/03"
+date[10]<- " 6/04"
+date[11]<- " 6/05"
+date[12]<- " 6/06"
+date[13]<- " 6/07"
+date[14]<- " 6/08"
+date[15]<- " 6/09"
 
+d = cbind(date,h.t.Berlin,l.t.Berlin)
 
-d = cbind(date,highest,lowest)
-
-doc <- read_html("https://www.accuweather.com/en/us/new-york-ny/10007/month/349727?monyr=6/01/2019")
+doc <- read_html("https://www.accuweather.com/en/ca/ottawa/k1p/month/55487?monyr=6/01/2019")
 doc %>% html_nodes(".date")
 doc %>% html_nodes(".date") %>% html_text()
 date<- doc %>% html_nodes(".date") %>% html_text()
+date<- substr(date,4,8)
+date[7]<- " 6/01"
+date[8]<- " 6/02"
+date[9]<- " 6/03"
+date[10]<- " 6/04"
+date[11]<- " 6/05"
+date[12]<- " 6/06"
+date[13]<- " 6/07"
+date[14]<- " 6/08"
+date[15]<- " 6/09"
 
-doc <- read_html("https://www.accuweather.com/en/us/new-york-ny/10007/month/349727?monyr=6/01/2019")
+doc <- read_html("https://www.accuweather.com/en/ca/ottawa/k1p/month/55487?monyr=6/01/2019")
 doc %>% html_nodes(".actual .large-temp")
 doc %>% html_nodes(".actual .large-temp") %>% html_text()
-highest<- doc %>% html_nodes(".large-temp") %>% html_text()
+h.t.Ottawa<- doc %>% html_nodes(".actual .large-temp") %>% html_text()
 
-doc <- read_html("https://www.accuweather.com/en/us/new-york-ny/10007/month/349727?monyr=6/01/2019")
+doc <- read_html("https://www.accuweather.com/en/ca/ottawa/k1p/month/55487?monyr=6/01/2019")
 doc %>% html_nodes(".small-temp")
 doc %>% html_nodes(".small-temp") %>% html_text()
-lowest<- doc %>% html_nodes(".small-temp") %>% html_text()
-lowest<- substr(lowest,2,4)
+l.t.Ottawa<- doc %>% html_nodes(".small-temp") %>% html_text()
+l.t.Ottawa<- substr(l.t.Ottawa,2,4)
 
-n = cbind(date,highest,lowest)
+c = cbind(date,h.t.Ottawa,l.t.Ottawa)
 
 doc <- read_html("https://www.accuweather.com/en/tw/taipei/226396/month/226396?monyr=6/01/2019")
 doc %>% html_nodes(".date")
 doc %>% html_nodes(".date") %>% html_text()
 date<- doc %>% html_nodes(".date") %>% html_text()
+date<- substr(date,4,8)
+date[7]<- " 6/01"
+date[8]<- " 6/02"
+date[9]<- " 6/03"
+date[10]<- " 6/04"
+date[11]<- " 6/05"
+date[12]<- " 6/06"
+date[13]<- " 6/07"
+date[14]<- " 6/08"
+date[15]<- " 6/09"
 
 doc <- read_html("https://www.accuweather.com/en/tw/taipei/226396/month/226396?monyr=6/01/2019")
 doc %>% html_nodes(".actual .large-temp")
 doc %>% html_nodes(".actual .large-temp") %>% html_text()
-highest<- doc %>% html_nodes(".actual .large-temp") %>% html_text()
+h.t.Taipei<- doc %>% html_nodes(".actual .large-temp") %>% html_text()
 
 doc <- read_html("https://www.accuweather.com/en/tw/taipei/226396/month/226396?monyr=6/01/2019")
 doc %>% html_nodes(".small-temp")
 doc %>% html_nodes(".small-temp") %>% html_text()
-lowest<- doc %>% html_nodes(".small-temp") %>% html_text()
-lowest<- substr(lowest,2,4)
+l.t.Taipei<- doc %>% html_nodes(".small-temp") %>% html_text()
+l.t.Taipei<- substr(l.t.Taipei,2,4)
 
-t = cbind(date,highest,lowest)
+t = cbind(date,h.t.Taipei,l.t.Taipei)
 
 doc <- read_html("https://www.accuweather.com/en/jp/tokyo/226396/month/226396?monyr=6/01/2019")
 doc %>% html_nodes(".date")
 doc %>% html_nodes(".date") %>% html_text()
 date<- doc %>% html_nodes(".date") %>% html_text()
+date<- substr(date,4,8)
+date[7]<- " 6/01"
+date[8]<- " 6/02"
+date[9]<- " 6/03"
+date[10]<- " 6/04"
+date[11]<- " 6/05"
+date[12]<- " 6/06"
+date[13]<- " 6/07"
+date[14]<- " 6/08"
+date[15]<- " 6/09"
 
 doc <- read_html("https://www.accuweather.com/en/jp/tokyo/226396/month/226396?monyr=6/01/2019")
 doc %>% html_nodes(".actual .large-temp")
 doc %>% html_nodes(".actual .large-temp") %>% html_text()
-highest<- doc %>% html_nodes(".actual .large-temp") %>% html_text()
+h.t.Tokyo<- doc %>% html_nodes(".actual .large-temp") %>% html_text()
 
 doc <- read_html("https://www.accuweather.com/en/jp/tokyo/226396/month/226396?monyr=6/01/2019")
 doc %>% html_nodes(".small-temp")
 doc %>% html_nodes(".small-temp") %>% html_text()
-lowest<- doc %>% html_nodes(".small-temp") %>% html_text()
-lowest<- substr(lowest,2,4)
+l.t.Tokyo<- doc %>% html_nodes(".small-temp") %>% html_text()
+l.t.Tokyo<- substr(l.t.Tokyo,2,4)
 
-j = cbind(date,highest,lowest)
+j = cbind(date,h.t.Tokyo,l.t.Tokyo)
 
-View(d)
+
+merge(t, j, by = "date")
+e = merge(t, j, by = "date")
+
+merge(d, c, by = "date")
+w = merge(d, c, by = "date")
+
+merge(e, w, by = "date")
+A = merge(e, w, by = "date")
